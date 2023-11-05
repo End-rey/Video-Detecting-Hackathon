d1e53e68566cf176ff1bc796cb4e9714b3f0ac5b
Denis <denisfedin1308@mail.ru>
2023-11-05 23:15:35 +0300

    Пофиксил траблы с версткой, сделал плавнее модальное окно и добавил обработку если камер нет

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import CameraComponentsList from "./components/CameraComponentsList";
 import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/modal/MyModal";
 import CameraAddForm from "./components/cameraAddForm/CameraAddForm";
+import NoCamera from "./components/noCamera/NoCamera";
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
     'http://webcam.anapa-official.ru:9999/player/?key=q1322qefasfrttg&cam=efb90850-93df-4bbb-ac51-9663342ee5b4',
     'http://webcam.anapa-official.ru:9999/player/?key=q1322qefasfrttg&cam=965aa513-a27d-4078-92b4-3ff104bc0622',
     'https://rtsp.me/embed/i93R6hin/?'
-  ])
+])
   const [cameraUrl, setCameraUrl] = useState({
     login: '',
     password: '',
@@ -78,9 +79,13 @@ function App() {
   return (
       <div className="App">
         <Navbar changeM={changeModalStatus} title={"Муниципальное образование города-курорта Анапа"}/>
+        {cameraList.length > 0
+        ? <CameraComponentsList cameraUrls={cameraList}/>
+        : <NoCamera/>
+        }
 
-        <CameraComponentsList cameraUrls={cameraList}/>
         <button onClick={pingServer}>Ping</button>
+
         {ping && <p>{ping}</p>}
         <MyModal cleanInput={cleanInput} visible={modal} setVisible={setModal}>
           <CameraAddForm modalStatus={modal}  cameraUrl={cameraUrl} setCameraUrl={setCameraUrl} add={addUrl}/>
