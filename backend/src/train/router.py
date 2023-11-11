5b633ebcdfe3fd7ac3a535806904cf01b451f616
End-rey <andreyis2017@gmail.com>
2023-11-11 17:11:54 +0300

    add base train model

---
+++ backend/src/train/router.py
@@ -0,0 +1,32 @@
+import base64
+from fastapi import APIRouter, Body, HTTPException, Response
+import numpy as np
+import os
+
+router = APIRouter()
+
+class DetectImage:
+    id: int
+    imageWithBox: str
+    rawImage: str
+    box: np.ndarray
+
+dir_train = "../dataset/train"
+if not os.path.exists(dir_train):
+    os.mkdir(dir_train)
+
+@router.post()
+async def post_image_to_train(response: Response, item: DetectImage = Body(...)):
+    try:
+        save_base64_image(item.rawImage, "uploaded_image.jpg")
+        return {"message": "Image saved successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error saving image: {str(e)}")
+
+def save_base64_image(base64_string, file_path):
+    _, data = base64_string.split(',')
+
+    binary_data = base64.b64decode(data)
+
+    with open(file_path, 'wb') as f:
+        f.write(binary_data)
